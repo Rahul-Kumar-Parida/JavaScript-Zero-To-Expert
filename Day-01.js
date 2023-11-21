035355a615a56915c919d021e5651020122da123
Rahul-Kumar-Parida <rahulkumarparida56@gmail.com>
2023-11-21 22:12:20 +0530

    add code

---
+++ Day-01.js
@@ -0,0 +1,25 @@
+// let js ="amazing"
+//         if (js === "amazing") alert("Javascript is for fun")
+
+        60+50+40+30-10;
+        console.log(60+50+40+30-10)
+
+        console.log("value")
+        console.log(34)
+
+        let FirstNamePerson
+        let first_name_person
+        // let 3years = 7  not include
+        //it contains letter number underscore dolar sign..
+        //do not use reserved keyward
+        //but we use
+        let $function = 9;
+        //you should start lowercase when naming a variable
+
+        let firstName = "Rahul"
+        console.log(firstName)
+        console.log(firstName)
+        console.log(firstName)
+
+        let myFirstJob ="programmer"
+        let myCurrentJob ="Teacher"
