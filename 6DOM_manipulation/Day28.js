19928b319fe548b259f78f95c7c991d2465b40d0
Rahul-Kumar-Parida <rahulkumarparida56@gmail.com>
2023-12-20 19:48:28 +0530

    add new code

---
+++ 6DOM_manipulation/Day28.js
@@ -0,0 +1,11 @@
+// "use strict";
+console.log(document.querySelector("#js").textContent);
+document.querySelector("#js").textContent = "😍 Correct Number";
+console.log(document.querySelector("#js").textContent);
+
+document.querySelector(".num").textContent = 679;
+
+document.querySelector(".number").value = 34;
+console.log((document.querySelector(".number").value = 34));
+
+document.querySelector('body').style.backgroundColor = 'green';
